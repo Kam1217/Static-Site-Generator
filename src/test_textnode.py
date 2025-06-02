6ae6d6d3ffb748e8a84a65b295cde1994ada25b9
Kam1217 <kamilaostroga@gmail.com>
2025-06-02 19:30:15 +0100

    test none url

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -2,12 +2,21 @@ import unittest
 
 from textnode import TextNode, TextType
 
-
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
         node = TextNode("This is a text node", TextType.BOLD)
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
+
+    def test_eq2(self):
+        node = TextNode("boot.dev",TextType.LINK, url="https://www.boot.dev/")
+        node2 = TextNode("boot.dev",TextType.LINK, url="https://www.boot.dev/")
+        self.assertEqual(node, node2)
+
+    def test_none_url(self):
+        node = TextNode("boot.dev",TextType.LINK)
+        node2 = TextNode("boot.dev",TextType.LINK, url="https://www.boot.dev/")
+        self.assertNotEqual(node, node2)
 
     def test_diff_type(self):
         node = TextNode("This is a text node", TextType.ITALIC)
@@ -18,10 +27,6 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This text is about dogs", TextType.BOLD)
         node2= TextNode("This is a different text about cats", TextType.BOLD)
         self.assertNotEqual(node, node2)
-        
-    def test_empty_url(self):
-        node = TextNode("", TextType.LINK)
-
 
 if __name__ == "__main__":
     unittest.main()
