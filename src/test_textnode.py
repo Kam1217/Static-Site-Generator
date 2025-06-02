bc8e7c9a860e0a60206e728e7ee0efa5ca29a8b6
Kam1217 <kamilaostroga@gmail.com>
2025-06-02 19:15:52 +0100

    Testing

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,19 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_diff_type(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is a text node", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+
+    def test_diff_text(self):
+        node = TextNode("This text is about dogs", TextType.BOLD)
+        node2= TextNode("This is a different text about cats", TextType.BOLD)
+        self.assertNotEqual(node, node2)
+        
+    def test_empty_url(self):
+        node = TextNode("", TextType.LINK)
+
 
 if __name__ == "__main__":
     unittest.main()
