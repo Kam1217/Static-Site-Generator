6f76fb8746969e8f836028de15d85fa2a770c29e
Kam1217 <kamilaostroga@gmail.com>
2025-06-03 10:02:03 +0100

    test leaf_to_html with a tag and link

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -6,3 +6,7 @@ class TestLeafNod(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.boot.dev/"})
+        self.assertEqual(node.to_html(), '<a href="https://www.boot.dev/">Click me!</a>')
