8746af00e7e6581d39afc7fef1298539358b2493
Kam1217 <kamilaostroga@gmail.com>
2025-06-04 10:33:42 +0100

    textnode to htmlnode for TextType.ITALIC

--- src/textnode_to_htmlnode.py
+++ src/textnode_to_htmlnode.py
@@ -8,5 +8,7 @@ def text_node_to_html_node(text_node):
     elif text_node.text_type == TextType.BOLD:
         return LeafNode("b", text_node.text)
     
-    
+    elif text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+
 
