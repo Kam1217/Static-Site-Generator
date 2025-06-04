dd115848f8ec50144df6ca0b2e7f6d833d60a234
Kam1217 <kamilaostroga@gmail.com>
2025-06-04 10:42:18 +0100

    Raise Exception if a TextNode is not a TextType

--- src/textnode_to_htmlnode.py
+++ src/textnode_to_htmlnode.py
@@ -19,5 +19,6 @@ def text_node_to_html_node(text_node):
     
     elif text_node.text_type == TextType.IMAGE:
         return LeafNode("img","",{"src": text_node.url, "alt": text_node.text})
-    
 
+    else:
+        raise Exception("TextNode is not a type of TextType")
