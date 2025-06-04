5cec79c695bec8a1db2311754d3e36e82dc8777b
Kam1217 <kamilaostroga@gmail.com>
2025-06-04 10:27:00 +0100

    textnode to htmlnode TextType.text returns LeafNode with raw text

--- src/textnode_to_htmlnode.py
+++ src/textnode_to_htmlnode.py
@@ -1,3 +1,6 @@
 from textnode import TextType, TextNode
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
