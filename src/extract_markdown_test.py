ba3d9d683a89e75b5a084c3a886ebb41ddfe6328
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 11:31:41 +0100

    test extract markdown links with one and multiple

--- src/extract_markdown_test.py
+++ src/extract_markdown_test.py
@@ -8,3 +8,15 @@ class TestLeafNod(unittest.TestCase):
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
         )
         self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")])
+
+    def test_multiple_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
