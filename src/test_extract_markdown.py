3ed52cc006da8fdf444f5d6337215f72dbaf1ca9
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 13:08:25 +0100

    put test at front of file name, fix extract markdown tests

--- src/test_extract_markdown.py
+++ src/test_extract_markdown.py
@@ -13,10 +13,10 @@ class TestLeafNod(unittest.TestCase):
         matches = extract_markdown_links(
             "This is text with a link [to boot dev](https://www.boot.dev)"
         )
-        self.assertListEqual([("to boot dev", "https://www.boot.dev")])
+        self.assertListEqual([("to boot dev", "https://www.boot.dev")], matches)
 
     def test_multiple_links(self):
         matches = extract_markdown_links(
             "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
         )
-        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")])
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
