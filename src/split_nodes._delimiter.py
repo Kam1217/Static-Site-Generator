09f05bee87eefa60a7cccba074693c516638ba87
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 09:53:38 +0100

    split by delimiter, check for unmatched delimiters, create new nodes from delimitere_chars

--- src/split_nodes._delimiter.py
+++ src/split_nodes._delimiter.py
@@ -1,8 +1,25 @@
-from textnode import TextType
+from textnode import TextType, TextNode
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    new_node = []
+    new_nodes = []
 
     for old_node in old_nodes:
         if old_node.text_type != TextType.TEXT:
-            new_node.append(old_node)
+            new_nodes.append(old_node)
+            continue
+
+    delimiter_chars = old_node.text.split(delimiter)
+
+    if len(delimiter_chars) % 2 == 0:
+        raise ValueError("Invalid markdown, formatted section not closed")
+    
+    for i, char in enumerate(delimiter_chars):
+        if char == "":
+            continue
+
+        if i % 2 == 0:
+            new_nodes.append(TextNode(char, TextType.TEXT))
+        else:
+            new_nodes.append(TextNode(char, text_type))
+
+    return new_nodes
