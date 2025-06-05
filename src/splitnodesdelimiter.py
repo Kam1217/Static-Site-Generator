73e4730801accd3eb0fa81129d5c78e6a94ea6f5
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 10:24:34 +0100

    fix various mix of delimters so test passes - indentation

--- src/splitnodesdelimiter.py
+++ src/splitnodesdelimiter.py
@@ -8,18 +8,18 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             new_nodes.append(old_node)
             continue
 
-    delimiter_chars = old_node.text.split(delimiter)
+        delimiter_chars = old_node.text.split(delimiter)
 
-    if len(delimiter_chars) % 2 == 0:
-        raise ValueError("Invalid markdown, formatted section not closed")
+        if len(delimiter_chars) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
     
-    for i, char in enumerate(delimiter_chars):
-        if char == "":
-            continue
+        for i, char in enumerate(delimiter_chars):
+            if char == "":
+                continue
 
-        if i % 2 == 0:
-            new_nodes.append(TextNode(char, TextType.TEXT))
-        else:
-            new_nodes.append(TextNode(char, text_type))
+            if i % 2 == 0:
+                new_nodes.append(TextNode(char, TextType.TEXT))
+            else:
+                new_nodes.append(TextNode(char, text_type))
 
     return new_nodes
