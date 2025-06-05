d5a4f84c74affde3dc402be40f2da24462946dcc
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:08:16 +0100

    Block type enum

---
+++ src/blocktype.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered"
+    ORDERED_LIST = "ordered"
