82c62fb6680c45d395dc9cb3b62540ec141dfd6e
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 09:38:04 +0100

    if old node is not a  TextType.TEXT add it to new node list

--- src/split_nodes._delimiter.py
+++ src/split_nodes._delimiter.py
@@ -1,1 +1,8 @@
+from textnode import TextType
+
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
+    new_node = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_node.append(old_node)
