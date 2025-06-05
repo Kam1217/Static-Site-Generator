77bed50bec2eb974bc432ebd9b67667175b87606
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 11:22:58 +0100

    Test extract markdown images function

---
+++ src/extract_markdown_test.py
@@ -0,0 +1,10 @@
+import unittest
+
+from extract_markdown_images_and_links import extract_markdown_images, extract_markdown_links
+
+class TestLeafNod(unittest.TestCase):
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
