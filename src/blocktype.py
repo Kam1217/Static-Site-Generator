63387bcf357ac24ddd20dc34053dd4d3aa254e6c
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:48:12 +0100

    block to block for lines

--- src/blocktype.py
+++ src/blocktype.py
@@ -9,15 +9,16 @@ class BlockType(Enum):
     ORDERED_LIST = "ordered"
 
 def block_to_block_type(block):
-
-    count = 0
-    for char in block:
-        if char == "#":
-            count += 1
-        else:
-            break   
-    if count >= 1 and count <= 6 and block[count] == " ":
+    
+    if block.startswith(("# ", "## ", "### ", "#### ", "##### ", "###### ")):
         return BlockType.HEADING
     
     if block.startswith("```") and block.endswith("```"):
         return BlockType.CODE
+    
+    lines = block.split("\n")
+    for line in lines:
+        if not line.startswith(">"):
+            return BlockType.PARAGRAPH
+    return BlockType.QUOTE
+    
