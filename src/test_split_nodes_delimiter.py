c008678e5a3aa76e8303c01498ad83b84aa00af7
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 10:11:12 +0100

    Test code delimiter

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -24,4 +24,12 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         ]
         self.assertEqual(new_nodes, expected)
 
-
+    def test_code_delimiter(self):
+        old_node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([old_node], "`", TextType.CODE)
+        expected = [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
