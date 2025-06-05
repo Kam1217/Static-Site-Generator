e1ed0289f26b21caf1c95a409423bd27ac765f2a
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 10:02:26 +0100

    test bold delimiter

--- src/test_split_nodes_delimiter.py
+++ src/test_split_nodes_delimiter.py
@@ -1,3 +1,15 @@
 import unittest
+from textnode import TextNode, TextType
+from splitnodesdelimiter import split_nodes_delimiter
 
 class TestSplitNodesDelimiter(unittest.TestCase):
+
+    def test_bold_delimiter(self):
+        old_node = TextNode("This is a **bold** text", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([old_node], "**", TextType.BOLD)
+        expected = [
+            TextNode("This is a ", TextType.TEXT),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text", TextType.TEXT),
+        ]
+        self.assertEqual(new_nodes, expected)
