edcbeb639dcbd4ca6b1da4d1f7beb59652d0aa6d
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:53:54 +0100

    block to block for ordered lists

--- src/blocktype.py
+++ src/blocktype.py
@@ -17,15 +17,25 @@ def block_to_block_type(block):
         return BlockType.CODE
     
     lines = block.split("\n")
+
     if block.startswith(">"):
         for line in lines:
             if not line.startswith(">"):
                 return BlockType.PARAGRAPH
         return BlockType.QUOTE
+    
     if block.startswith("- "):
         for line in lines:
             if not line.startswith("- "):
                 return BlockType.PARAGRAPH
         return BlockType.UNORDERED_LIST
     
+    if block.startswith("1. "):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
     
