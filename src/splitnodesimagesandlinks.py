bc1695ef72a4852a64b999089eb1e184b85db9d1
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 11:41:24 +0100

    split nodes function for images and links setup

---
+++ src/splitnodesimagesandlinks.py
@@ -0,0 +1,7 @@
+from extract_markdown_images_and_links import extract_markdown_images, extract_markdown_links
+
+def split_nodes_images(old_nodes):
+    pass
+
+def split_nodes_links(old_nodes):
+    pass
