9e85002f8cf2fddc1ec8831df5936289f6e4dd7f
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 16:06:08 +0100

    test block to paragraph

--- src/test_blocktype.py
+++ src/test_blocktype.py
@@ -22,8 +22,10 @@ class TestSplitNodesDelimiter(unittest.TestCase):
     def test_block_to_ordered(self):
         block = "1. list, 2. list"
         self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
-
-
+    
+    def test_block_to_paragraph(self):
+        block = "This is a paragraph"
+        self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
     
 
 
