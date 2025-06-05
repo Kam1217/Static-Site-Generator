57d5232a8c939ebb18446fea9caceaa7d7840372
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:28:56 +0100

    block to block type for headings

--- src/blocktype.py
+++ src/blocktype.py
@@ -7,3 +7,14 @@ class BlockType(Enum):
     QUOTE = "quote"
     UNORDERED_LIST = "unordered"
     ORDERED_LIST = "ordered"
+
+def block_to_block_type(block):
+    
+    count = 0
+    for char in block:
+        if char == "#":
+            count += 1
+        else:
+            break   
+    if count >= 1 and count <= 6 and block[count] == " ":
+        return BlockType.HEADING
