90e5d05741f3ee76381690da6a38569b4ca3e718
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:59:49 +0100

    test block to code

--- src/test_blocktype.py
+++ src/test_blocktype.py
@@ -6,6 +6,11 @@ class TestSplitNodesDelimiter(unittest.TestCase):
     def test_block_to_heading(self):
         block = "# heading"
         self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    
+    def test_block_to_code(self):
+        block = "```code```"
+        self.assertEqual(block_to_block_type(block), BlockType.CODE)
+
 
     
 
