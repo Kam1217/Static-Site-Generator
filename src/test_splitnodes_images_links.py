6d9221c8ce2847464e427197e89e77435f9a8c3e
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 13:52:25 +0100

    test split links

--- src/test_splitnodes_images_links.py
+++ src/test_splitnodes_images_links.py
@@ -5,11 +5,11 @@ from splitnodesimagesandlinks import split_nodes_image, split_nodes_link
 class TestHTMLNode(unittest.TestCase):
     
     def test_split_images(self):
-        node = TextNode(
+        old_node = TextNode(
             "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
             TextType.TEXT,
         )
-        new_nodes = split_nodes_image([node])
+        new_nodes = split_nodes_image([old_node])
         self.assertListEqual(
             [
                 TextNode("This is text with an ", TextType.TEXT),
@@ -20,4 +20,20 @@ class TestHTMLNode(unittest.TestCase):
                 ),
             ],
             new_nodes,
+        )
+
+    def test_split_links(self):
+        old_node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.TEXT
+        )
+        new_nodes = split_nodes_link([old_node])
+        self.assertListEqual(
+            [
+                TextNode("This is text with a link ", TextType.TEXT),
+                TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev")
+            ],
+            new_nodes
         )
