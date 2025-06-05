55d47df4f90a2ad20f2fb8a6085b014ac1785293
Kam1217 <kamilaostroga@gmail.com>
2025-06-05 15:58:25 +0100

    test block to heading

--- src/test_blocktype.py
+++ src/test_blocktype.py
@@ -2,3 +2,12 @@ import unittest
 from blocktype import BlockType, block_to_block_type
 
 class TestSplitNodesDelimiter(unittest.TestCase):
+    
+    def test_block_to_heading(self):
+        block = "# heading"
+        self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+
+    
+
+
+
