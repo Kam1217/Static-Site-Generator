891c2b6facfd59f13b4e0588080ef92f1215cbac
Kam1217 <kamilaostroga@gmail.com>
2025-06-06 13:40:41 +0100

    extract file function

--- src/extract_title.py
+++ src/extract_title.py
@@ -1,2 +1,18 @@
+   #open file - in main
+    #read file and convert to markddown to be passed in
 def extract_title(markdown):
+
+    title = ""
+
+    markdown_lines = markdown.split("\n")
+
+    for line in markdown_lines:
+        if line.startswith("# "):
+            strp_line = line[2: ]
+            title = strp_line.strip()
+            break
     
+    if title == "":
+        raise Exception("<h1> title not found")
+    
+    return title
