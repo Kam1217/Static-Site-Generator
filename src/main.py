a624262026788fb6ef458e102c382ac7dbfc7e72
Kam1217 <kamilaostroga@gmail.com>
2025-06-06 14:59:43 +0100

    call generate page func in main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,14 @@
 import os
 from textnode import TextType, TextNode
 from copy_content_from_source import copy_content_from_source
+from generate_page import generate_page
 
 
 def main():
     source = os.path.join(os.getcwd(), "static") 
     destination = os.path.join(os.getcwd(), "public")
     copy_content_from_source(source, destination)
+    generate_page("content/index.md", "template.html", "public/index.html")
+
 main()
 
