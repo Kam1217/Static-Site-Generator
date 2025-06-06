fd281562e601792fd6f721ee2e50ff38a9dfaa18
Kam1217 <kamilaostroga@gmail.com>
2025-06-06 13:58:03 +0100

    test extract title heading returns in correct format and error is raised if there is no heading

--- src/test_extract_title.py
+++ src/test_extract_title.py
@@ -2,3 +2,12 @@ import unittest
 from extract_title import extract_title
 
 class TestSplitNodesDelimiter(unittest.TestCase):
+
+    def test_h1_heading(self):
+        heading = "# Heading"
+        self.assertEqual(extract_title(heading), "Heading")
+
+    def test_h1_not_found_exception(self):
+        with self.assertRaises(Exception):
+            text = "text with no heading"
+            extract_title(text)
