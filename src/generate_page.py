e2bb935598f918b211b8baa20eb52f352b4b0d14
Kam1217 <kamilaostroga@gmail.com>
2025-06-06 14:21:09 +0100

    generate page function - read markdown and template files , extract title and change makrdown to html

--- src/generate_page.py
+++ src/generate_page.py
@@ -1,3 +1,20 @@
+from markdown_to_html import markdown_to_html_node 
+from htmlnode import *
+from extract_title import extract_title
 
-def generate_page(from_path, template_path,dest_path):
-    pass
+def generate_page(from_path, template_path, dest_path):
+    
+    print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+
+    from_file = open(from_path)
+    template_file = open(template_path)
+    markdown = from_file.read()
+    template = template_file.read()
+
+    html = markdown_to_html_node(markdown).to_html()
+    title = extract_title(markdown)
+
+    
+
+
+    
